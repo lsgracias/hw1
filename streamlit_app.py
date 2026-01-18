8c18ea4875e56bf4cc9fadc767f3faccd66ae264
Lauren Gracias <lgracias2022@gmail.com>
2026-01-18 21:28:12 +0000

    final_hw1

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,21 +28,20 @@ else:
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
-    try: 
     # Let the user upload a file via `st.file_uploader`.
-        uploaded_file = st.file_uploader(
-            "Upload a document (.txt or .pdf)", type=("txt", "pdf")
-        )
+    uploaded_file = st.file_uploader(
+        "Upload a document (.txt or .pdf)", type=("txt", "pdf")
+    )
 
     # Ask the user for a question via `st.text_area`.
-        question = st.text_area(
-            "Now ask a question about the document!",
-            placeholder="Can you give me a short summary?",
-            disabled=not uploaded_file,
-        )
+    question = st.text_area(
+        "Now ask a question about the document!",
+        placeholder="Can you give me a short summary?",
+        disabled=not uploaded_file,
+    )
 
-        if uploaded_file and question:
-
+    if uploaded_file and question:
+        try:
         # Process the uploaded file and question.
             file_extension = uploaded_file.name.split('.')[-1]
             
@@ -71,5 +70,5 @@ else:
         # Stream the response to the app using `st.write_stream`.
             st.write_stream(stream)
 
-    except Exception as e:
-        st.error("Invalid API Key, please try again.")
+        except Exception as e:
+            st.error("Invalid API Key, please try again.")
